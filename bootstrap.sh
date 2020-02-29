9656dbb3efdf7949abccf4bbf33dd3f390f5cd4d
kevin Martínez <kevinccbsg@gmail.com>
2020-02-29 12:45:17 +0100

    Commands to provision elastic search with their configuration

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,3 +2,24 @@
 
 echo "Configure script"
 
+# Elastic search
+
+## Import elastic PGP key
+
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+
+## update packages
+sudo apt-get update
+
+## Installing from the APT repository
+sudo apt-get install apt-transport-https
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
+
+sudo apt-get update && sudo apt-get install elasticsearch
+
+## init with systemd
+
+cp /vagrant/elasticsearch/elasticsearch.yml /etc/elasticsearch/
+sudo systemctl daemon-reload
+sudo systemctl enable elasticsearch.service
+sudo systemctl start elasticsearch.service
